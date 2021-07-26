27833f8a2467b37cced95603b64391b6b3abebb5
Ahaider60 <alihaiderizvi.you@gmail.com>
2021-07-27 04:02:36 +0500

    Phase 4:Intial Template

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./todo.css";
 const Todo = () => {
-	return <div></div>;
+	return (
+		<>
+			<div className='main-div'>
+				<div className='child-div'>
+					<figure>
+						<figcaption>Add List Items</figcaption>
+					</figure>
+
+					<div className='addItems'>
+						<input type='text' className='' placeholder='Add Items' />
+						<i className='fa fa-plus add-btn'></i>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Todo;
